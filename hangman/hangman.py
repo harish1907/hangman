6d9c5632cf1449baaa1a456a200dc85513e14212
harish chaudhary <67159642+harish1907@users.noreply.github.com>
2022-05-30 11:53:52 +0530

    Add files via upload

---
+++ hangman/hangman.py
@@ -0,0 +1,38 @@
+import random
+from re import A
+from hangman_design import *
+from words import *
+
+chosen_word = random.choice(word_list)
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+display=[]
+lives=6
+
+for i in chosen_word:
+  display.append('_')
+
+print(display)
+print(stages[lives])
+
+while True:
+  guess = input("Guess a letter: ").lower()
+  for i in range(len(display)):
+    if chosen_word[i]==guess:
+      display[i]=guess
+
+  if guess not in chosen_word:
+    lives-=1
+    print(stages[lives])
+
+  if lives==0:
+    print('Game Over')
+    break
+  
+  if '_' in display:
+    print(display)
+  else:
+    print(display)
+    print('You win') 
+    break
